3a5e9e6bc6b62aff5186fd9ad468c10ee4a56d58
Saptarshi <136781153+MeRishi07@users.noreply.github.com>
2024-12-28 22:44:18 +0530

    Delete src directory

--- src/main/java/com/ecommerce/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.dao;
-
-import com.ecommerce.entity.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserDao extends CrudRepository<User, String> {
-}
